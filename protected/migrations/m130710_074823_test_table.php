2bc635cffd81c55fa4275da20264ee7cf9f793e2
channyeinkyaw <channyeinkyaw@gmail.com>
2013-07-10 17:01:05 +0900

    update

---
+++ protected/migrations/m130710_074823_test_table.php
@@ -0,0 +1,31 @@
+<?php
+
+class m130710_074823_test_table extends CDbMigration
+{
+	public function up()
+	{
+      $this->createTable('tbl_news', array(
+            'id' => 'pk',
+            'title' => 'string NOT NULL',
+            'content' => 'text',
+        ));
+	}
+
+	public function down()
+	{
+//		echo "m130710_074823_test_table does not support migration down.\n";
+//		return false;
+      $this->dropTable('tbl_news');
+	}
+
+	/*
+	// Use safeUp/safeDown to do migration with transaction
+	public function safeUp()
+	{
+	}
+
+	public function safeDown()
+	{
+	}
+	*/
+}
