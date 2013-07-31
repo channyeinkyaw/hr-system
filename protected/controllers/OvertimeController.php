b0f21620ab12db912c1c9c90fbda7545167dd22f
channyeinkyaw <channyeinkyaw@gmail.com>
2013-07-31 17:01:59 +0900

    update

--- protected/controllers/OvertimeController.php
+++ protected/controllers/OvertimeController.php
@@ -3,18 +3,26 @@
   class OvertimeController extends Controller
   {
 
-      public function actionIndex()
-      {
-          echo "index";
-      }
-
-      public function actionPage($alias)
-      {
-          echo "Page is $alias.";
-      }
-      public function actionTest()
-      {
-          //echo $this->createUrl('website/page', 'alias' => 'about');
-          //echo $this->createAbsoluteUrl('website/page', 'alias' => 'test');
+      
+      public function actionGridView(){
+        $rawData = Yii::app()->db->createCommand()
+        ->select('*')
+        ->from('working_hour')
+        ->queryAll();
+        $dataProvider=new CArrayDataProvider($rawData
+        //, array(
+        //'sort'=>array(
+        //'attributes'=>array(
+        //'working_date' => 'working_date',
+        //),
+        //),
+        //
+        //'pagination'=>array(
+        //'pageSize'=>10,
+        //),
+        //
+        //)
+        );
+        $this->render('site/pages/overtime.php', array('dataProvider'  => $dataProvider));
       }
   }
--- protected/views/site/pages/overtime.php
+++ protected/views/site/pages/overtime.php
@@ -9,17 +9,57 @@ $this->breadcrumbs=array(
 <h1>Overtime</h1>
 
 <?php
-    $users = Yii::app()->db->createCommand()
-    ->select('*')
-    ->from('working_hour')
-    ->queryAll();
-echo "<pre>"; 
-var_dump($users);
-echo "</pre>";
-
-echo CHtml::link(CHtml::encode($users->ID));
+//    $users = Yii::app()->db->createCommand()
+//    ->select('*')
+//    ->from('working_hour')
+//    ->queryAll();
+//echo "<pre>"; 
+//var_dump($users);
+//echo "</pre>";
+//
+//echo CHtml::link(CHtml::encode($users->ID));
 //    echo "<pre>";
 //    print_r (Yii::app()->db);
 //    echo "</pre>";
-//$users=>ID;
+
+$rawData = Yii::app()->db->createCommand()
+    ->select('*')
+    ->from('working_hour')
+    ->queryAll();
+$dataProvider=new CArrayDataProvider($rawData
+//, array(
+//'sort'=>array(
+//'attributes'=>array(
+//'working_date' => 'working_date',
+//),
+//),
+//
+//'pagination'=>array(
+//'pageSize'=>10,
+//),
+//
+//)
+);
+
+$this->widget('zii.widgets.grid.CGridView', array(
+
+'dataProvider'=>$dataProvider,
+//    'columns'=>array('ID','working_date'),
+//    'htmlOptions'=>array('width'=>'10px'),
+    'columns'=>array(
+    array(
+'name'  => 'ID',
+'header'=> 'ID',
+'value' => $data['ID'],
+'htmlOptions'=>array('width'=>'10px')),
+array(
+'name'  => 'working_date',
+'header'=> 'Working Date',
+'value' => $data['working_date'],
+'htmlOptions'=>array('width'=>'10px')),
+)
+
+));
+
+
 ?>
