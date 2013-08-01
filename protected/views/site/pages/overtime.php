03363ce4ad0e9b92aae25e08eb1badb77bd86258
channyeinkyaw <channyeinkyaw@gmail.com>
2013-08-01 17:01:22 +0900

    update

--- protected/views/site/pages/overtime.php
+++ protected/views/site/pages/overtime.php
@@ -9,57 +9,41 @@ $this->breadcrumbs=array(
 <h1>Overtime</h1>
 
 <?php
-//    $users = Yii::app()->db->createCommand()
-//    ->select('*')
-//    ->from('working_hour')
-//    ->queryAll();
-//echo "<pre>"; 
-//var_dump($users);
-//echo "</pre>";
-//
-//echo CHtml::link(CHtml::encode($users->ID));
-//    echo "<pre>";
-//    print_r (Yii::app()->db);
-//    echo "</pre>";
 
 $rawData = Yii::app()->db->createCommand()
-    ->select('*')
-    ->from('working_hour')
+    ->select('wh.ID, UPPER(pi.first_name) AS Name, 
+      wh.working_date AS Date, SEC_TO_TIME(TIME_TO_SEC(wh.end_time) - TIME_TO_SEC(sh.std_end)) AS Overtime')
+    ->from('working_hour wh')
+    ->join('standard_hour sh','sh.id=1')
+    ->join('personal_info pi','pi.ID=wh.personal_id')
+    ->order('Overtime')
+    //->where('wh.personal_id=2')
     ->queryAll();
-$dataProvider=new CArrayDataProvider($rawData
-//, array(
+
+$dataProvider=new CArrayDataProvider($rawData, array('pagination'=>array('pageSize'=>10,)));
+
 //'sort'=>array(
 //'attributes'=>array(
 //'working_date' => 'working_date',
 //),
 //),
 //
-//'pagination'=>array(
-//'pageSize'=>10,
 //),
 //
-//)
-);
 
-$this->widget('zii.widgets.grid.CGridView', array(
+$this->widget('zii.widgets.grid.CGridView', array('dataProvider'=>$dataProvider,'columns'=>array()));
 
-'dataProvider'=>$dataProvider,
 //    'columns'=>array('ID','working_date'),
 //    'htmlOptions'=>array('width'=>'10px'),
-    'columns'=>array(
-    array(
-'name'  => 'ID',
-'header'=> 'ID',
-'value' => $data['ID'],
-'htmlOptions'=>array('width'=>'10px')),
-array(
-'name'  => 'working_date',
-'header'=> 'Working Date',
-'value' => $data['working_date'],
-'htmlOptions'=>array('width'=>'10px')),
-)
-
-));
-
-
+//    array(
+//'name'  => 'ID',
+//'header'=> 'ID',
+//'value' => $data['ID'],
+//'htmlOptions'=>array('width'=>'10px')),
+//array(
+//'name'  => 'working_date',
+//'header'=> 'Working Date',
+//'value' => $data['working_date'],
+//'htmlOptions'=>array('width'=>'10px')),
+//)
 ?>
